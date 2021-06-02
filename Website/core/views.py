d470722feabfc8d7eee241843d8ab34b9638156d
Kashish Jain <kashishjain32@gmail.com>
2021-06-02 12:18:22 +0530

    Cleaning of code

--- Website/core/views.py
+++ Website/core/views.py
@@ -19,10 +19,24 @@ import glob
 import pathlib
 import json
 from django.contrib.auth.decorators import login_required
-  
+import shutil
 #TEST_IMAGE_PATHS = sorted(list(PATH_TO_TEST_IMAGES_DIR.glob("*.jpg")))
 media_url = settings.MEDIA_URL
 static_url = settings.STATIC_URL
+
+def delete():
+    folders=['media']
+    for folder in folders :
+        folder = folder
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                     shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
 
 def loginPage(request):
     if request.user.is_authenticated:
@@ -93,6 +107,7 @@ def main(request):
         print(os.listdir(settings.MEDIA_ROOT))
         files = os.listdir(settings.MEDIA_ROOT)
         df_total = pd.DataFrame()
+        context = {}
         for file in files:
             df=pd.read_csv(settings.MEDIA_ROOT+'/'+file)
             #df=df.drop(['id'],axis=1)
@@ -121,21 +136,28 @@ def main(request):
                     print(df.iloc[row]['diagnosis'])
             # geeks_object = df.to_html()
             # return HttpResponse(geeks_object)
-
             df_total = pd.concat([df_total, df])
+
             print(df)
             # df.close()
-            df.to_csv(file[:-4]+'_predicted.csv')
+            # df.to_csv('Predicted/'+file[:-4]+'_predicted.csv')
+            # json_records = df.reset_index().to_json(orient ='records')
+            # data = []
+            # data = json.loads(json_records)
+            # context[file] = data
+
         print(df_total)
         json_records = df_total.reset_index().to_json(orient ='records')
         data = []
         data = json.loads(json_records)
-        context = {'d': data}
+        context['files'] = files
+        print(context)
+        delete()
         return render(request, 'table.html', context)
 
 
 
     return render(request, 'bulk.html')
-    
+
 def index(request):
     return redirect('login')
